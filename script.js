034bb548828c72159f2ce67ded5d7b0c950f0932
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-08-11 00:07:47 -0700

    started html/css

---
+++ script.js
@@ -0,0 +1,14 @@
+//create objects for each item 
+
+//use 3 arrays to hold objects for each category 
+//insert object into css grid
+
+var fruitArray = [];
+var veggieArray = [];
+var dairyArray = [];
+
+//store each item into shopping cart
+var cartArray = [];
+
+
+//Onclick, add item to shopping cart 
